9af4716d771fea83039a1f1505b7f742a257bff7
Aromal Nambiar <aromalndr@gmail.com>
2023-04-06 16:18:48 +0530

    first commit

---
+++ src/ecommerce/store/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def home(request):
+    context = {}
+    return render(request, 'store/index.html', context)
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
+
