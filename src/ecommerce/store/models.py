1377fc4f40b6dd1fdbda934b120145e519834b5b
Aromal Nambiar <aromalndr@gmail.com>
2023-04-09 22:46:24 +0530

    add to admin panel

--- src/ecommerce/store/models.py
+++ src/ecommerce/store/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-#customer model
+#customer 
 
 class Customer(models.Model):
     user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
