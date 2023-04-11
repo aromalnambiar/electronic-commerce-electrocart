26531a745092298c0741f4b3fd967eff06fead30
Aromal Nambiar <aromalndr@gmail.com>
2023-04-11 21:27:28 +0530

    today commit

--- src/ecommerce/store/views.py
+++ src/ecommerce/store/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
 from .models import *
 
+
+
 #home
 
 def home(request):
     product = Product.objects.all()
-    context = {
-        'products' : product
-    }
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    
+    else:
+        items = []
+        order = {'get_cart_total' : 0, 'get_cart_items' : 0}    
+    
+    context = {'items' : items, 'order': order, 'product': product}
     return render(request, 'store/index.html', context)
 
 #store
