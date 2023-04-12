5312591f306a6f104c02d8c7b8c7bac016fbd168
Aromal Nambiar <aromalndr@gmail.com>
2023-04-12 22:00:14 +0530

    todays commit

--- src/ecommerce/store/views.py
+++ src/ecommerce/store/views.py
@@ -23,8 +23,19 @@ def home(request):
 
 def store(request):
     product = Product.objects.all()
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    
+    else:
+        items = []
+        order = {'get_cart_total' : 0, 'get_cart_items' : 0}    
+        
     context = {
-        'products' : product
+        'products' : product,
+        'items': items,
+        'order': order,
     }
     return render(request, 'store/store.html', context)
 
